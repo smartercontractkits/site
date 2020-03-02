2dc1d6f2eb8395eea03d2d92dfbd76891efac80d
RX93 <61353723+NuAnon@users.noreply.github.com>
2020-03-02 16:34:29 +0100

    Update core.js

--- core.js
+++ core.js
@@ -3,35 +3,23 @@
 
 //Sequential For Loop
 //Processing of Array
-function loop(array, position) {
+function loop(data) {
 	var output = "";
-	for(var i = 0; i < array.length; i++) {
-    	output += " <a href=\"" + array[i][0] + "\">" + array[i][position] + "</a>";
+	for(var i = 0; i < data.length; i++) {
+    	output += " <a href=\"" + data[i][0] + "\">" + data[i][1] + "</a>";
     }
-    document.body.innerHTML = output;   
+    document.body.innerHTML = "<span>" + output + "</span>";   
 }
-////////////////////
-//Scrolling Function
-function updownio() {
-	var former = 0;
-	var locale = 0;
-	//Downscroll ++, Upscroll --
-	(window.scrollY > former) ? protem++ : protem--;
-	former = window.scrollY;
-	sessionStorage.setItem("status", protem);
-	//Value Numerical Boundaries
-	//Max Set to Eight Times [J]
-	protem = (Math.min(40, Math.max(1, protem)));
-	//Determining Array Position
-	locale = (((Math.ceil(protem/8)*8)-8)/8);
-	loop(contents[locale], 1);
+/////////////////////
+//PageChange Function
+function turnpage(event) {
+	if (event.target.tagName == "BODY") {
+		(state < 4) ? state++ : state = 0;
+		loop(contents[state]);
+	}
 }
 
 
-//Set SessionStorage
-sessionStorage.setItem("status", 0);
-var protem = sessionStorage.getItem("status");
-////////////////////
-//HTML DOM Structure
-window.addEventListener("scroll", updownio);
-loop(contents[protem], 1);
+var state = 0;
+loop(contents[state]);
+document.body.addEventListener("click", turnpage);
