cb345ee2869c725cfd8b4899a3d1e55e671c84f9
NuAnon <61353723+NuAnon@users.noreply.github.com>
2020-03-03 15:59:19 +0100

    Update text.js

--- text.js
+++ text.js
@@ -10,34 +10,39 @@ var platform; //SNSs Page
 var alliance; //Team Page
 
 contents =	[
-/////////////
+/////////////////
 mainpage =	[//Under 250 Chars
-			["https://bit.ly/38avY3R",	"<img src=\"imgs/hexagon.png\"></img> <b>nuLink</b> a decentralised oracle system for Ethereum smart contracts"]
-			],
+		["https://bit.ly/38avY3R",	"<img src=\"imgs/hexagon.png\"></img> <b>nuLink</b> a decentralised oracle system for Ethereum smart contracts"]
+		],
 
 pamphlet =	[//ISO 639-1 Codes
-			["https://bit.ly/2wcP0cq",	"<img src=\"imgs/de.png\"></img>"],
-			["docs/whitepaper.pdf",		"<img src=\"imgs/en.png\"></img>"],
-			["https://bit.ly/2wcP0cq",	"<img src=\"imgs/fr.png\"></img>"],
-			["https://bit.ly/2wcP0cq",	"<img src=\"imgs/it.png\"></img>"]
-			],
+		["#",				"<img src=\"imgs/xx.png\"></img>"],
+		["https://bit.ly/2wcP0cq",	"<img src=\"imgs/de.png\"></img>"],
+		["https://bit.ly/2wcP0cq",	"<img src=\"imgs/en.png\"></img>"],
+		["https://bit.ly/2wcP0cq",	"<img src=\"imgs/es.png\"></img>"],
+		["https://bit.ly/2wcP0cq",	"<img src=\"imgs/fr.png\"></img>"],
+		["https://bit.ly/2wcP0cq",	"<img src=\"imgs/hi.png\"></img>"],
+		["https://bit.ly/2wcP0cq",	"<img src=\"imgs/pt.png\"></img>"],
+		["https://bit.ly/2wcP0cq",	"<img src=\"imgs/ru.png\"></img>"],
+		["https://bit.ly/2wcP0cq",	"<img src=\"imgs/zh.png\"></img>"]
+		],
 
 exchange =	[//List Major Ones
-			["https://bit.ly/384Zpny",	"<p>ForkDelta</p>"],
-			["https://bit.ly/2PvqNET",	"<p>Saturn DEX</p>"],
-			["https://bit.ly/2uFBbTy",	"<p>Uniswap</p>"]
-			],
+		["https://bit.ly/384Zpny",	"<p>ForkDelta</p>"],
+		["https://bit.ly/2PvqNET",	"<p>Saturn DEX</p>"],
+		["https://bit.ly/2uFBbTy",	"<p>Uniswap</p>"]
+		],
 
 platform =	[//One Per Service
-			["https://bit.ly/2HZVzl9",	"<img src=\"imgs/discord.png\"></img>"],
-			["https://bit.ly/2PsE01g",	"<img src=\"imgs/github.png\"></img>"],
-			["https://bit.ly/2T6PZ78",	"<img src=\"imgs/telegram.png\"></img>"],
-			["https://bit.ly/2I5oKn0",	"<img src=\"imgs/twitter.png\"></img>"]
-			],
+		["https://bit.ly/2HZVzl9",	"<img src=\"imgs/discord.png\"></img>"],
+		["https://bit.ly/2PsE01g",	"<img src=\"imgs/github.png\"></img>"],
+		["https://bit.ly/2T6PZ78",	"<img src=\"imgs/telegram.png\"></img>"],
+		["https://bit.ly/2I5oKn0",	"<img src=\"imgs/twitter.png\"></img>"]
+		],
 
 alliance =	[//Link One Social
-			["https://bit.ly/2wQWfqG",	"@ArjunMGujin,"],
-			["https://bit.ly/-------",	"@SKY"]
-			]
-/////////////
-			];
+		["https://bit.ly/2wQWfqG",	"@ArjunMGujin,"],
+		["https://bit.ly/-------",	"@SKY"]
+		]
+/////////////////
+		];
