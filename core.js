fffb81d20c2853dcf066516c8cb8fb9dfdf3a0b0
NuAnon <61353723+NuAnon@users.noreply.github.com>
2020-03-03 14:16:15 +0100

    Update core.js

--- core.js
+++ core.js
@@ -13,8 +13,10 @@ function loop(data) {
 /////////////////////
 //PageChange Function
 function turnpage(event) {
-	if (event.target.tagName == "BODY") {
-		(state < 4) ? state++ : state = 0;
+	var half = (event.target.offsetHeight/2); 
+	if (event.target.tagName !== "span") {
+		(event.clientY < half) ? state-- : state++;
+		state = Math.min(4, Math.max(0, state));
 		loop(contents[state]);
 	}
 }
@@ -23,3 +25,6 @@ function turnpage(event) {
 var state = 0;
 loop(contents[state]);
 document.body.addEventListener("click", turnpage);
+var div = document.createElement("div");
+document.body.appendChild(div);
+div.innerHTML = "&#8595;";
